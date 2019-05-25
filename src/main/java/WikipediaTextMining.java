d08a669780ae89a0a3c687c4260898e4803095e3
Siva Sivasubramoniam Jayaram <sivajrm@gmail.com>
2019-05-25 05:20:28 -0700

    With filtered revision-id, timestamp and contributor having contributions > 1

--- src/main/java/WikipediaTextMining.java
+++ src/main/java/WikipediaTextMining.java
@@ -1,17 +1,27 @@
 import java.io.*;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
-import java.util.regex.Matcher;
+import java.util.Arrays;
 import java.util.regex.Pattern;
 
+import com.sun.xml.internal.bind.v2.TODO;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.catalyst.expressions.GenericRow;
+import org.apache.spark.sql.functions.*;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import scala.Tuple2;
+import scala.collection.mutable.WrappedArray;
 
-import static org.apache.spark.sql.functions.col;
+import static org.apache.spark.sql.functions.*;
 
 public class WikipediaTextMining {
 
@@ -35,9 +45,10 @@ public class WikipediaTextMining {
                 .format("xml")
                 .option("rowTag", "page")
                 .load(basePath+"\\sample.xml");
-         Dataset<Row> df2 = df.select(col("title"), col("id"), col("revision"));
-         //getMinorTagsCount(df2);
-        getAtMostFiveUrlLinks(df2);
+         //Dataset<Row> df2 = df.select(col("title"), col("id"), col("revision"));
+         //getMinorTagsCount(df);
+         //getAtMostFiveUrlLinks(df);
+        getOnePlusContributorAlongWithRevisionId(df);
         ctx.stop();
         System.exit(0);
     }
@@ -63,6 +74,72 @@ public class WikipediaTextMining {
         //System.out.println("Filtered rows with <= 5 url count:" + filteredRows.count());
     }
 
+    static void getOnePlusContributorAlongWithRevisionId(Dataset<Row> dataset){
+        Dataset<Row> df = (Dataset<Row>) dataset.select(dataset.col("revision")).toDF();
+
+        df.persist();
+
+        Dataset<Row> filteredDf = df.select(df.col("revision.id").as("rev-id"), df.col("revision.timestamp").as("rev-ts"),
+                df.col("revision.contributor.id").as("contrib-id"), df.col("revision.contributor.ip").as("contrib-ip"),
+                df.col("revision.contributor.username").as("contrib-u-name")).toDF();
+
+        df.unpersist();
+        filteredDf.persist();
+
+        //for sample xml
+        //filtered 2901 after grouping by contrib-id | contrib-u-name
+        //         958                    contrib-ip
+        //         3859                   together
+        // >1      833
+        Dataset<Row> filteredDfWithCount = (Dataset<Row>) filteredDf.withColumn("count", org.apache.spark.sql.functions.lit(1));
+
+        filteredDf.unpersist();
+        filteredDfWithCount.persist();
+
+        Dataset<Row> gt1ContributionDF = (Dataset<Row>) filteredDfWithCount.groupBy(filteredDfWithCount.col("contrib-id"),
+                filteredDfWithCount.col("contrib-u-name"),filteredDfWithCount.col("contrib-ip"))
+                .agg(sum(filteredDfWithCount.col("count")), collect_list("rev-ts").as("rev-ts-list"),
+                        collect_list("rev-id").as("rev-id-list")).toDF();
+
+        filteredDfWithCount.unpersist();
+        gt1ContributionDF.persist();
+
+        Dataset<Row> result =  (Dataset<Row>) gt1ContributionDF.filter("sum(count) > 1").toDF();
+
+        gt1ContributionDF.unpersist();
+        result.persist();
+
+        //TODO:Sort the revision id based on the descending order of timestamp
+        //result.toJavaRDD().map(row -> sortRevision(row));
+    }
+
+    private static Row sortRevision(Row row){
+        String[] timeStampArr = (String[]) row.getAs(4);
+        Long[] revisionIdArr = (Long[]) row.getAs(5);
+        DateTime[] dateTimesArr = new DateTime[timeStampArr.length];
+        DateTimeFormatter format = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+
+        HashMap<DateTime, Integer> tsIndexMap = new HashMap<>();
+        for(int index = 0; index < timeStampArr.length; index++){
+            dateTimesArr[index] = format.parseDateTime(timeStampArr[index]);
+            tsIndexMap.put(dateTimesArr[index], index);
+        }
+
+        Arrays.sort(dateTimesArr, new Comparator<DateTime>(){
+            public int compare(DateTime d1, DateTime d2){
+                return d2.compareTo(d1);
+            }
+        });
+
+        Long[] revisionSortedArr = new Long[timeStampArr.length];
+        for(int index = 0; index < timeStampArr.length; index++){
+             int oldIndex = tsIndexMap.get(dateTimesArr[index]);
+             revisionSortedArr[index] = revisionIdArr[oldIndex];
+        }
+
+        return row;
+    }
+
     private static void addToOutput(StringBuilder sb, Row row){
         String rowString = String.format("%s \t\t %s \n",row.getAs("id").toString(), row.getAs("title").toString());
         sb.append(rowString);
@@ -81,7 +158,7 @@ public class WikipediaTextMining {
         }
         return true;
     }
-    
+
     class PageDetails{
         Integer pageId;
         String pageTitle;
